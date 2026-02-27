575ec654f1c6ffca85279594a29918dfb375802d
nounfve <rootastgrfvbgh564778hj@gmail.com>
2026-02-27 20:31:11 +0800

    revert use mod_use_all

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,13 @@ mod into_lifetime;
 mod macros;
 mod singleton;
 
+pub use context_func::*;
+pub use into_lifetime::*;
+pub use macros::*;
+pub use singleton::*;
+
+/// disabled since rust-analyzer not follow using statement in macro
+/// bug
+#[cfg(false)]
 #[sutils_macro::mod_use_all]
-struct UseAll;
+struct UseAll; 
