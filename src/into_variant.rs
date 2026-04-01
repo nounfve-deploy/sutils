bd81e5f0acd9fa56771d91a303bd0c2e91a140a6
nounfve <rootastgrfvbgh564778hj@gmail.com>
2026-04-02 00:45:44 +0800

    wider impl

--- src/into_variant.rs
+++ src/into_variant.rs
@@ -1,37 +1,43 @@
 #[allow(nonstandard_style)]
-pub trait IntoResult
+pub trait IntoResult<T>
 where
     Self: Sized,
 {
-    fn Ok<E>(self) -> Result<Self, E>;
-    fn Err<O>(self) -> Result<O, Self>;
+    fn Ok<E>(self) -> Result<T, E>;
+    fn Err<O>(self) -> Result<O, T>;
 }
 
-impl<T> IntoResult for T {
-    fn Ok<E>(self) -> Result<Self, E> {
-        Ok(self)
+impl<T, This> IntoResult<T> for This
+where
+    This: Into<T>,
+{
+    fn Ok<E>(self) -> Result<T, E> {
+        Ok(self.into())
     }
 
-    fn Err<O>(self) -> Result<O, Self> {
-        Err(self)
+    fn Err<O>(self) -> Result<O, T> {
+        Err(self.into())
     }
 }
 
 #[allow(nonstandard_style)]
-pub trait IntoOption
+pub trait IntoOption<T>
 where
     Self: Sized,
 {
-    fn Some(self) -> Option<Self>;
-    fn None(&self) -> Option<Self>;
+    fn Some(self) -> Option<T>;
+    fn None(&self) -> Option<T>;
 }
 
-impl<T> IntoOption for T {
-    fn Some(self) -> Option<Self> {
-        Some(self)
+impl<T, This> IntoOption<T> for This
+where
+    This: Into<T>,
+{
+    fn Some(self) -> Option<T> {
+        Some(self.into())
     }
 
-    fn None(&self) -> Option<Self> {
+    fn None(&self) -> Option<T> {
         None
     }
 }
